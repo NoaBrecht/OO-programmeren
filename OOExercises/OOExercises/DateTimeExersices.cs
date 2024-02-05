3e66974844759acf18d1743591b704df0fbe88d7
NoaBrecht <99073951+NoaBrecht@users.noreply.github.com>
2024-02-05 16:34:26 +0100

    H01

---
+++ OOExercises/OOExercises/DateTimeExersices.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOExercises
+{
+    internal class DateTimeExersices
+    {
+        public static void ShowSubmenu()
+        {
+            Console.WriteLine("Uit te voeren oefening?");
+            Console.WriteLine("1. H10-Clock");
+            Console.WriteLine("2. H10-Birthday");
+            Console.WriteLine("3. ");
+            Console.WriteLine("4. ");
+            Console.WriteLine("5. ");
+            Console.WriteLine("6. ");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:H10_Clock();
+                    break;
+                case 2:
+                    H10_Birthday();
+                    break;
+                default:
+                    Console.WriteLine("Foutieve optie");
+                    break;
+            }
+        }
+        public static void H10_Clock()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine($"{DateTime.Now.ToLongTimeString()}");
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+        public static void H10_Birthday()
+        {
+
+        }
+    }
+}
---
+++ OOExercises/OOExercises/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace OOExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welkom bij de oefeningen van Objectgeoriënteerd Programmeren!");
+            Console.WriteLine("Topic van de uit te voeren oefening?");
+            Console.WriteLine("1. DateTime");
+            Console.WriteLine("2. Properties en access modifiers");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    DateTimeExersices.ShowSubmenu();
+                    break;
+                default:
+                    Console.WriteLine("Foutieve optie");
+                    break;
+            }
+        }
+    }
+}
