d3ed5b4487a01bebb94e5021ba5d822d3b86c357
Noa Van den Berghe <vandenberghenoa1@gmail.com>
2024-04-29 14:51:53 +0200

    labo_17

---
+++ labo_16/oef_01/ExceptionHandling.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oef_01
+{
+    internal class ExceptionHandling
+    {
+        public static void ToonSubmenu()
+        {
+            Console.WriteLine("");
+            DemonstreerFoutafhandelingWeekdagenZonderException();
+            
+        }
+        private static void DemonstreerFoutafhandelingWeekdagenZonderException()
+        {
+            string[] arr = new string[5];
+            arr[0] = "Vrijdag";
+            arr[1] = "Maandag";
+            arr[2] = "Dinsdag";
+            arr[3] = "Woensdag";
+            arr[4] = "Donderdag";
+
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine(arr[i].ToString());
+            }
+        }
+    }
+}
